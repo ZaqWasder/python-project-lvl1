879f5084553110793d2dc55fc894089a5e57533a
Artem Budarin <buldozer199@gmail.com>
2025-02-23 02:20:56 +0300

    added game brain-prime

---
+++ brain_games/games/prime.py
@@ -0,0 +1,21 @@
+from random import randint
+
+MIN_NUMBER = 1
+MAX_NUMBER = 100
+RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def get_challenge():
+    num = randint(MIN_NUMBER, MAX_NUMBER)
+    question = f'{num}'
+    answer = 'yes' if is_prime(num) else 'no'
+
+    return (question, answer)
+
+
+def is_prime(num):
+    for i in range(2, num // 2):
+        if num % i == 0:
+            return False
+
+    return True
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,10 @@
+import brain_games.games.prime as prime
+from brain_games.utils import run_game
+
+
+def main():
+    run_game(prime)
+
+
+if __name__ == "__main__":
+    main()
